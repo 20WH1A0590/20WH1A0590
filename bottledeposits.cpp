f733f13a997a33c01903a89b964c1a1a0540ae86
Tarannum <94288233+20WH1A0590@users.noreply.github.com>
2022-12-26 18:02:26 +0530

    Add files via upload

---
+++ bottledeposits.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int a,b; cout << "Enter the number of containers: ";
+    cin >> a >> b;
+    cout << "$" << a*0.10 + b*0.10; 
+    return 0;
+}
